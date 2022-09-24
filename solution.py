2584e1311ea4c172cba94889123c314308a43cd8
ca1878 <72635836+ca1878@users.noreply.github.com>
2022-09-24 14:05:57 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -57,8 +57,9 @@ def webServer(port=13331):
 
       #Fill in end
 
-  serverSocket.close()
-  sys.exit()  # Terminate the program after sending the corresponding data
+  #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+  #serverSocket.close()
+  #sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
   webServer(13331)
