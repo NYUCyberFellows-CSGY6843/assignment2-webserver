34a8fb5b43b028dd12bf4aeb6445d8741b650f39
Jen Smith <js12020@nyu.edu>
2022-09-24 20:10:21 -0700

    Update solution.py
    removed the \ from the end of line 31

--- solution.py
+++ solution.py
@@ -28,7 +28,7 @@ def webServer(port=13331):
       f = open(filename[1:], #fill in start #fill in end)
       #fill in end
       
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
+      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
       #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
       #Content-Type above is an example on how to send a header as bytes
       #Fill in end
